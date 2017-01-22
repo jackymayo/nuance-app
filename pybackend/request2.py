fec5b4764aa03beaae73b6eb42b02fb6bc97ab9b
Wesley Chung <wesley.chung2@gmail.com>
2017-01-22 10:15:07 -0500

    Data for scatterplots

--- pybackend/request2.py
+++ pybackend/request2.py
@@ -24,37 +24,113 @@ import csv
 
 ### Testing
 #raw = r.text
-raw = """I propose to consider the question, "Can machines think?" This should begin with
-definitions of the meaning of the terms "machine" and "think." The definitions might be
-framed so as to reflect so far as possible the normal use of the words, but this attitude is
-dangerous, If the meaning of the words "machine" and "think" are to be found by
-examining how they are commonly used it is difficult to escape the conclusion that the
-meaning and the answer to the question, "Can machines think?" is to be sought in a
-statistical survey such as a Gallup poll. But this is absurd. Instead of attempting such a
-definition I shall replace the question by another, which is closely related to it and is
-expressed in relatively unambiguous words."""
+# raw = """I propose to consider the question, "Can machines think?" This should begin with
+# definitions of the meaning of the terms "machine" and "think." The definitions might be
+# framed so as to reflect so far as possible the normal use of the words, but this attitude is
+# dangerous, If the meaning of the words "machine" and "think" are to be found by
+# examining how they are commonly used it is difficult to escape the conclusion that the
+# meaning and the answer to the question, "Can machines think?" is to be sought in a
+# statistical survey such as a Gallup poll. But this is absurd. Instead of attempting such a
+# definition I shall replace the question by another, which is closely related to it and is
+# expressed in relatively unambiguous words."""
+
+### Get data from text.json
+with open('text.json') as f:
+    all_data = json.load(f)
 
 
+all_data = all_data["test"][0]
+
+print(all_data)
+
+
+
+### Use word2vec to get 'thought vectors' for words
+# load the Google's pre-trained model (too big for GitHub)
+# model = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+# for i in range(10):
+#     if str(i) in all_data:
+#         raw = all_data[str(i)]
+#         print(raw)
+#         # Remove punctuation except for apostrophe and make lowercase
+#         text = re.sub(r'[^\w\s\']', '', raw)
+#         text = text.lower()
+#         words = text.split()
+#
+#         # Count words
+#         counts = collections.Counter(words)
+#         df = pandas.DataFrame.from_dict(counts, orient='index')
+#
+#         word_vectors = []
+#         words = []
+#         # Get the word vectors for the words used in the text
+#         for word in list(df.index):
+#             try:
+#                 word_vectors.append(model[word])
+#                 words.append(word)
+#             except KeyError:
+#                 print("not found", word)
+#                 pass
+#
+#         df_word = pandas.DataFrame(word_vectors)
+#         df_word.index = words
+#
+#         # save word vectors for later use
+#         with open('word_vectors' + str(i) + '.p', 'wb') as fp:
+#             pickle.dump(df_word, fp)
+
+
+for i in range(10):
+    ## load saved word vectors
+    with open('word_vectors' + str(i) + '.p', 'rb') as fp:
+        df_word = pickle.load(fp)
+    print(df_word)
+
+    tsne_json = []
+
+    if len(df_word.index) > 1:
+        # Reduce the dimensions to only 2d (or 3d, change n_componenents)
+        model_tsne = TSNE(n_components=2, random_state=1)
+        dim_reduced = model_tsne.fit_transform(df_word)
+        print(dim_reduced)
+
+        minx = min(dim_reduced[:,0])
+        miny = min(dim_reduced[:,1])
+
+        for label, x, y in zip(df_word.index, dim_reduced[:,0], dim_reduced[:, 1]):
+            tsne_json.append({"word" : label, "x": x+minx+1, "y": y+miny+1})
+
+    else:
+        #for label, x, y in zip(df_word.index, df_word[:, 0], df_word[:, 1]):
+        tsne_json.append({"word": df_word.index[0], "x": 3, "y": 3})
+
+    with open('tsne' + str(i) + '.json', 'w') as f:
+        json.dump(tsne_json, f)
+
+
+    # # Plot the dimension-reduced vectors in 2d
+    # plt.scatter(dim_reduced[:,0], dim_reduced[:,1])
+    # for label, x, y in zip(df_word.index, dim_reduced[:,0], dim_reduced[:, 1]):
+    #    plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+    # plt.show()
+
+
+
+
+
+
+
+
+
+################ OLD STUFF ##################
 ### splits returned string into words and computes metrics
-# Remove punctuation except for apostrophe and make lowercase
-text = re.sub(r'[^\w\s\']', '', raw)
-text = text.lower()
-words = text.split()
 
-# Count words
-counts = collections.Counter(words)
-print(counts)
-
-df = pandas.DataFrame.from_dict(counts, orient='index')
-# df.plot(kind='bar')
-# plt.show()
 
 
 # Save everything
 # df.to_csv("WordCount_" + time.strftime("%d%m%y_%H%M") + ".csv")
 # with open("RawText_" + time.strftime("%d%m%y_%H%M") + ".txt", "w") as text_file:
 #     text_file.write(raw)
-
 
 
 ### Get synonyms for a word
@@ -65,57 +141,3 @@ df = pandas.DataFrame.from_dict(counts, orient='index')
 #     return r.json()
 
 #get_synonyms("small")
-
-
-### Use word2vec to get 'thought vectors' for words
-# load the Google's pre-trained model (too big for GitHub)
-# model = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-# word_vectors = []
-# words = []
-## Get the word vectors for the words used in the text
-# for word in list(df.index):
-#     try:
-#         word_vectors.append(model[word])
-#         words.append(word)
-#     except KeyError:
-#         print("not found", word)
-#         pass
-#
-# df_word = pandas.DataFrame(word_vectors)
-# df_word.index = words
-
-# save word vectors for later use
-# with open('word_vectors.p', 'wb') as fp:
-#    pickle.dump(df_word, fp)
-
-# load saved word vectors
-with open('word_vectors.p', 'rb') as fp:
-    df_word = pickle.load(fp)
-
-#print(df_word)
-
-# Reduce the dimensions to only 2d (or 3d, change n_componenents)
-model_tsne = TSNE(n_components=2, random_state=1)
-dim_reduced = model_tsne.fit_transform(df_word)
-print(dim_reduced)
-
-tsne_json = []
-for label, x, y in zip(df_word.index, dim_reduced[:,0], dim_reduced[:, 1]):
-    tsne_json.append({"word" : label, "x": x, "y": y})
-
-with open('tsne.json', 'w') as f:
-    json.dump(tsne_json, f)
-
-print(tsne_json)
-
-# with open('tsne.csv', 'w') as fp:
-#     writer = csv.writer(fp, delimiter=',')
-#     for line in zip(df_word.index, dim_reduced[:,0], dim_reduced[:, 1]):
-#         writer.writerow(line)
-
-#Plot the dimension-reduced vectors in 2d
-# plt.scatter(dim_reduced[:,0], dim_reduced[:,1])
-# for label, x, y in zip(df_word.index, dim_reduced[:,0], dim_reduced[:, 1]):
-#    plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
-# plt.show()
-
